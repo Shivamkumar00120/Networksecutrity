6fe0563043fa1518334c54528c0cf7088de753dd
Shivam kumar <shivamkumar032324@gmail.com>
2026-07-30 17:20:43 +0530

    setup.py file is ready

---
+++ setup.py
@@ -0,0 +1,35 @@
+'''
+
+'''
+
+from setuptools import setup, find_packages
+from typing import List
+def get_requirements() -> List[str]:
+    """
+    This function will return the list of requirements
+    """
+    requirement_lst: List[str] = []
+    try:
+        with open("requirements.txt",'r') as file:
+            #read line form the file 
+            lines=file.readlines()
+            ## proceed each line
+            for line in lines:
+                requirement=line.strip()
+                ## ignore empty lines and -e .
+                if requirement and requirement != '-e .':
+                    requirement_lst.append(requirement)      
+    except FileNotFoundError:
+        print("requirements.txt file not found.")
+    return requirement_lst
+print(get_requirements())
+
+
+setup(
+    name="NetworkSecurity",
+    version="0.0.1",
+    author="Shivam kumar",
+    author_email="shivamkumar032324@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements()
+)
